d5a9b2c16b81c61ff1115ba46183742588699b28
nintwentydo <platform@prismmedia.io>
2024-12-27 14:31:57 +1100

    update start

--- src/start.sh
+++ src/start.sh
@@ -2,12 +2,23 @@
 
 set -uxo pipefail
 
-LOG=/app/tabbyAPI.log
+LOG1=/app/tabbyAPI.log
+LOG2=/app/handler.log
 
+# Start main.py
 (
   cd /app
-  python3.11 main.py >> $LOG 2>&1
+  python3.11 main.py >> $LOG1 2>&1
+) &
+
+# Start handler.py
+(
+  cd /app
+  python3.11 -u handler.py >> $LOG2 2>&1
 ) &
 
 set +x
-sleep 1 && tail -f $LOG
+
+# Allow processes to start and then tail logs
+sleep 1
+tail -f $LOG1 $LOG2
